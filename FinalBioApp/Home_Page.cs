d0f418743271118ebc38631697085edead982db5
Macen Vargas <mvargas9517@live.com>
2018-10-08 17:09:56 -0500

    Current state of the bio app that I have created

---
+++ FinalBioApp/Home_Page.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace FinalBioApp
+{
+    public class Home_Page : ContentPage
+    {
+        public Home_Page()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
